29acc8eda1b49939fb1d86cb9d8e9309f7594185
Blaon <iloveelevation@gmail.com>
2020-02-04 13:45:36 +0100

    random number

--- static/js/game.js
+++ static/js/game.js
@@ -1,3 +1,14 @@
-function sprite_movement() {
+var bluebar = document.getElementById('blue')
+function dropBox(bar){
+    bar.style.left = random_x().toString()+'px';
+    do {
+        bar.style.top+=
+    }
+}
 
+
+
+
+function random_x() {
+    return Math.floor(Math.random()*(97-1))+1;
 }
