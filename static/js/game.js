6003c8b9b1f4fc3b98b5c836a03c3cc1740fcfb2
Vivien Walter <walter.vivien.k@gmail.com>
2020-02-05 10:28:01 +0100

    finetuned collision detection

--- static/js/game.js
+++ static/js/game.js
@@ -4,22 +4,19 @@ var progbar = {
 };
 
 var enemies = [
-    { left: randomizer(), top: 0 }
+    { left: randomizer(), top: 0, background: "url('/static/assets/blueblock2.png')" }
 ];
 
 document.onkeydown = function (e) {
-    console.log(e.key);
     if (progbar.left >= 0 && progbar.left <= 77.5) {
         if (e.key === 'ArrowLeft') {
             if (progbar.left > 0) {
-                console.log(progbar.left)
                 progbar.left = progbar.left - 0.5;
             }
         }
 
         else if (e.key === 'ArrowRight') {
             if (progbar.left < 77.5) {
-                console.log(progbar.left)
                 progbar.left = progbar.left + 0.5;
                 }
         }
@@ -36,7 +33,7 @@ function drawHero() {
 function drawEnemies() {
     document.getElementById('enemies').innerHTML = ""
     for(var i = 0 ; i < enemies.length ; i++ ) {
-        document.getElementById('enemies').innerHTML += `<div class='enemy' style='left:${enemies[i].left}%; top:${enemies[i].top}%'></div>`;
+        document.getElementById('enemies').innerHTML += `<div class='enemy' style="left:${enemies[i].left}%; top:${enemies[i].top}%; background-image:${enemies[i].background}"></div>`;
     }
 }
 
@@ -48,26 +45,25 @@ function moveEnemies() {
 
 
 function randomizer() {
-    return Math.floor(Math.random()*120)+1;
+    return Math.floor(Math.random()*100)+1;
+}
+
+
+function choose_random_sprite() {
+    var myArray = ['url(\'/static/assets/blueblock2.png\')', 'url(\'/static/assets/blueblock2.png\')', 'url(\'/static/assets/redblock.png\')', 'url(\'/static/assets/orangeblock.png\')'];
+    return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
 
 function collisionDetection() {
     for (var enemy = 0; enemy < enemies.length; enemy++) {
-        console.log('progbar')
-        console.log(progbar.top)
-        console.log(progbar.left)
-        console.log(progbar.left + 37)
-        console.log('enemy')
-        console.log(enemies[enemy].top)
-        console.log(enemies[enemy].left)
+
             if (
                 enemies[enemy].left >= progbar.left &&
-                enemies[enemy].left <= progbar.left + 37 &&
+                enemies[enemy].left <= progbar.left + 22 &&
                 enemies[enemy].top >= progbar.top &&
-                enemies[enemy].top <= progbar.top + 10
+                enemies[enemy].top <= progbar.top + 15
             ) {
-                console.log("tácsitácsi")
                 enemies.splice(enemy, 1);
             }
         }
@@ -79,7 +75,6 @@ function end_of_screen() {
         if (
                     enemies[enemy].top > 90
                 ) {
-                    console.log("end if screen")
                     enemies.splice(enemy, 1);
                 }
     }
@@ -90,16 +85,13 @@ var i = 0
 
 function gameLoop() {
             i++
-    console.log(i)
                 if (i%10 === 0) {
-                    console.log('new enemy')
-                    enemies.push({left: randomizer(), top: 0})
+                    enemies.push({left: randomizer(), top: 0, background: choose_random_sprite()})
                 }
-                    console.log('move')
-                    setTimeout(gameLoop, 500)
+                    setTimeout(gameLoop, 300)
                     moveEnemies();
-                    collisionDetection();
                     drawEnemies();
+                    collisionDetection();
                     end_of_screen();
 
 
